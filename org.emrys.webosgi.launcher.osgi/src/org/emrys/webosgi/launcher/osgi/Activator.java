8318d4de2beaa1bc2e43fe6e4080afc7e541f979
xhzhang1006 <xhzhang1006@gmail.com>
2013-12-13 01:34:28 +0000

    对OSGi embedded模式下的自动启动web bundles机制进行了改进。兼容domino和普通的OSGi环境两种方式。这两者区别在于Bridge Servlet init方法是否在OSGi启动时调用。

--- org.emrys.webosgi.launcher.osgi/src/org/emrys/webosgi/launcher/osgi/Activator.java
+++ org.emrys.webosgi.launcher.osgi/src/org/emrys/webosgi/launcher/osgi/Activator.java
@@ -3,6 +3,10 @@
  */
 package org.emrys.webosgi.launcher.osgi;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.emrys.webosgi.launcher.internal.OSGiFwkLauncher;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -12,10 +16,48 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator implements BundleActivator {
 
-	static BundleContext BUNDLE_CTX;
-
 	public void start(BundleContext bundleCtx) throws Exception {
-		this.BUNDLE_CTX = bundleCtx;
+		// Send a http request to active all web bundles when this launcher
+		// plugin actived. We cann't do it in bridge servlet's init() method,
+		// for IBM Domino server will not start the servlet when launching even
+		// we set load-on-startup with true in extension point.
+		if (OSGiFwkLauncher.isFwkOSGiEmbedded())
+			sendWebOSGiStartRequest(bundleCtx);
+	}
+
+	public void sendWebOSGiStartRequest(BundleContext bundleCtx) {
+		// Send a asyn http url to this OSGi WebContainer to active all web
+		// bundles if fwk web context path configured.
+		try {
+			final String host = bundleCtx.getProperty("webosgi.host");
+			// String ctxPathConfigured =
+			// bundleCtx.getProperty("webosgi.contextPath");
+			if (host != null && host.trim().length() > 0) {
+				new Thread(new Runnable() {
+					public void run() {
+						try {
+							// Domino not use global servlet context path, so
+							// just use the path in extension point of
+							// HttpService.
+							HttpURLConnection urlConn = (HttpURLConnection) (new URL(
+									host
+											+ BridgeHttpServlet.BRIDGE_SERVLET_MAP_PATH)
+									.openConnection());
+							urlConn.setConnectTimeout(600000);
+							// We do not care the response.
+							int httpStatus = urlConn.getResponseCode();
+							System.out.println("WebOSGi Auto-Start OK.");
+						} catch (Exception e) {
+							new Exception("WebOSGi Auto-Start Request failed.",
+									e).printStackTrace();
+						}
+					}
+				}).start();
+			}
+		} catch (Exception e) {
+			new Exception("WebOSGi Auto-Start Request failed.", e)
+					.printStackTrace();
+		}
 	}
 
 	public void stop(BundleContext bundleCtx) throws Exception {
